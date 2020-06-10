c868147f7c505db6eebc49843de77373c3688b51
Jennifer Beaudry <jbeaudry@swin.edu.au>
2020-06-10 10:59:40 +0930

    Move ASSH vs STEM code to here

--- analysis/Code_Graveyard.R
+++ analysis/Code_Graveyard.R
@@ -165,4 +165,67 @@ p
 # code to save figure. If using it for slides, increase the 'base_size' from 12 to 18. 
 #ggsave(here::here("figs", "aclevels_bar.png"))
 
+###################### EFFORTS TO SPLIT OUT ASSH VS STEM RESPONDENTS ######
+
+#### OS Experience ####
+```{r os_experience_pie_assh, echo = FALSE, eval = FALSE}
+
+# data for just ASSH respondents
+title = "What is your experience with open science practices? HAS respondents (n = %d)"
+d_assh <- df %>% filter(assh == "ASSH")
+
+# pie chart
+colours <- brewer.pal(4, "Blues")
+p_assh <- simple_pie_chart(the_data = d_assh, label_var = "OverallExp",
+                           the_title = title, 
+                           colours,the_quantity = "percent")
+p_assh
+
+# data for just STEM respondents
+title = "What is your experience with open science practices? STEM respondents (n = %d)"
+d_stem <- df %>% filter(assh == "STEM")
+
+# pie chart
+colours <- brewer.pal(4, "Blues")
+p_stem <- simple_pie_chart(the_data = d_stem, label_var = "OverallExp",
+                           the_title = title, 
+                           colours,the_quantity = "percent")
+p_stem
+```
+
+#### PREREG EXPERIENCE ####
+
+```{r prereg_exp_pie_chart_assh, echo = FALSE, warning = TRUE, eval=FALSE}
+
+d_assh <- df %>% filter(assh == "ASSH") 
+title = "What is your experience with preregistration? HAS respondents (n = %d)"
+
+# pie chart
+colours <- brewer.pal(4, "Blues")
+p_assh <- simple_pie_chart(the_data = d_assh, label_var = "PreregExp1", 
+                           the_title = title, 
+                           colours,the_quantity = "percent")
+p_assh
+
+rm(title)
+```
+
+#### PREREG IMPORTANCE ####
+
+```{r prereg_imp_ex_pie_chart_assh, echo = FALSE, warning = TRUE, eval = FALSE}
+
+title = "How important is it that researchers preregister their studies? HAS respondents (n = %d)"
+
+d_assh <- df %>% 
+  filter(assh == "ASSH") %>% 
+  filter(PreRegImp != "Researchers in my discipline do not conduct research studies")
+
+# pie chart
+colours <- brewer.pal(4, "Blues")
+p_assh <- simple_pie_chart(the_data = d_assh, label_var = "PreRegImp", 
+                           the_title = title, 
+                           colours,the_quantity = "percent")
+p_assh
+```
+
 
